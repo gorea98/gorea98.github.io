5847203c6d3ddbd716ee8d752d5e592db83e75cf
gorea98 <56277442+gorea98@users.noreply.github.com>
2019-11-04 15:51:41 +0200

    Add files via upload

--- gyro/gyro.js
+++ gyro/gyro.js
@@ -4,11 +4,40 @@ window.addEventListener("deviceorientation", on_gyro_data_uab);
 window.addEventListener("devicemotion", on_acc_data_uab);
 
 
+function desenare(unghi_x, unghi_y)
+{
+	// obtinem referinta la canvas
+	
+	var canvas = document.getElementById("canvas");
+	
+	// obtinem referinta la context
+	var context = canvas.getContext("2d");
+	
+	// stergem canvasul
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	
+	// incepem sa construim o cale(path)
+	context.beginPath();
+	
+	var r = 10;
+	var x = canvas.width / 2 + unghi_x / 90 * (canvas. width / 2 - r);
+	var y = canvas.width / 2 + unghi_y / 90 * (canvas. width / 2 - r);
+	
+	// adaugam cercul la cale
+	context.arc(x,y,r,0,2*Math.PI);
+
+	// desenam calea
+	context.Stroke();
+}
+
 function on_gyro_data_uab(e)
 {
 	document.getElementById("id_alpha").innerHTML = Math.round(e.alpha * 100)/100;
 	document.getElementById("id_beta").innerHTML = Math.round(e.beta * 100)/100;
 	document.getElementById("id_gamma").innerHTML = Math.round(e.gamma * 100)/100;
+	
+	
+	desenare(e.beta, e.gamma);
 	
 }
 
@@ -30,3 +59,4 @@ function on_acc_data_uab(e)
 	document.getElementById("id_rot_y").innerHTML = Math.round(rot_y * 100)/100;
 	document.getElementById("id_rot_z").innerHTML = Math.round(rot_z * 100)/100;
 }
+

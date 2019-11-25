4cdac0cf9283e1cdd350eb750a604c5a65301f51
gorea98 <56277442+gorea98@users.noreply.github.com>
2019-11-25 13:08:55 +0200

    Add files via upload

--- worker/desenare.js
+++ worker/desenare.js
@@ -1,10 +1,11 @@
-document.getElementById("id_logic").innerHTML = "2019.11.25.5";
+document.getElementById("id_logic").innerHTML = "2019.11.25.6";
 document.getElementById("id_start").addEventListener("click",start);
 document.getElementById("id_stop").addEventListener("click",stop);
 
 var timer_id;
 var unghi = {};
 unghi.valoare = 0;
+var muncitor;
 
 function desenare(unghi)
 {
@@ -41,10 +42,10 @@ function start()
 	
 	timer_id = setInterval(desenare, 20, unghi);
 	
-	var muncitor = new Worker("prime.js");
+	muncitor = new Worker("prime.js");
 	muncitor.onmessage = function(e)  {
 		
-		document.getElementById("id_prime").innerHTML = e.data;
+		document.getElementById("id_prim").innerHTML = e.data;
 	}
 	
 }
@@ -55,4 +56,5 @@ function stop()
 	document.getElementById("id_stop").disabled ="true";
 	
 	clearInterval(timer_id);
+	muncitor.postMessage("stop");
 }

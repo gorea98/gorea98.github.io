24e0dfbc8f5a543ef1d492d7ce151469e04e18f4
gorea98 <56277442+gorea98@users.noreply.github.com>
2019-12-02 13:46:22 +0200

    Update svg.js

--- HTML/svg/svg.js
+++ HTML/svg/svg.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic = 2019.12.02.1";
+document.getElementById("id_logic_version").innerHTML = "Logic = 2019.12.02.2";
 
 window.addEventListener("deviceorientation", on_gyro_data_uab); 
 window.addEventListener("devicemotion", on_acc_data_uab); 
@@ -11,8 +11,8 @@ function deseneaza(unghi_x, unghi_y)
 
 	var r = circle.getAttribute("r");
 	// adaugam un cerc la cale
-	var x = unghi_x / 90 * (svg.width / 2 - r) + svg.width / 2;
-	var y = unghi_y / 90 * (svg.height / 2 - r) + svg.height / 2;
+	var x = unghi_x / 90 * (svg.width.animVal.value / 2 - r) + svg.width.animVal.value / 2;
+	var y = unghi_y / 90 * (svg.height.animVal.value / 2 - r) + svg.height.animVal.value / 2;
 
 	// actualizam pozitia cercului
 	circle.setAttribute("cx", x);

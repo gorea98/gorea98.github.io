36f3fc6622d2bbc28dfd32d08649710ecfe01761
gorea98 <56277442+gorea98@users.noreply.github.com>
2019-12-02 13:18:08 +0200

    Add files via upload

--- HTML/svg/svg.js
+++ HTML/svg/svg.js
@@ -1,37 +1,36 @@
-document.getElementById("id_logic").innerHTML = "Logic = 2019.12.02.0";
+document.getElementById("id_logic_version").innerHTML = "Logic = 2019.12.02.1";
 
-window.addEventListener("deviceorientation", on_gyro_data_uab);
-window.addEventListener("devicemotion", on_acc_data_uab);
+window.addEventListener("deviceorientation", on_gyro_data_uab); 
+window.addEventListener("devicemotion", on_acc_data_uab); 
 
-
-function desenare(unghi_x, unghi_y)
+function deseneaza(unghi_x, unghi_y)
 {
-	// obtinem referinta la canvas
-	
+	// obtinem context grafic
 	var circle = document.getElementById("id_circle");
 	var svg = document.getElementById("id_svg");
-	circle.getAttribute("r");
-	
-	var r = 10;
-	var x =  unghi_x / 90 * (svg.width / 2 - r) + svg.width / 2;
-	var y =  unghi_y / 90 * (svg.height / 2 - r) + svg.height / 2;
-	
+
+	var = circle.getAttribute("r");
+	// adaugam un cerc la cale
+	var x = unghi_x / 90 * (svg.width / 2 - r) + svg.width / 2;
+	var y = unghi_y / 90 * (svg.height / 2 - r) + svg.height / 2;
+
+	// actualizam pozitia cercului
 	circle.setAttribute("cx", x);
 	circle.setAttribute("cy", y);
 }
 
 function on_gyro_data_uab(e)
-{
-	
-	
-	
-	desenare(e.beta, e.gamma);
-	
+{	
+	deseneaza(e.beta, e.gamma);
 }
 
 function on_acc_data_uab(e)
 {
 	var acc = e.accelerationIncludingGravity;
+	
+	var rot_x = Math.atan(acc.x / acc.z) * 180 / Math.PI;
+	var rot_y = Math.atan(acc.y / acc.z) * 180 / Math.PI;	
+}
 	
 	
 	

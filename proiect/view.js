5e90dcdff097210c814c5a6c0220a5413877c14c
gorea98 <56277442+gorea98@users.noreply.github.com>
2020-02-02 01:44:38 +0200

    Update view.js

--- proiect/view.js
+++ proiect/view.js
@@ -1,7 +1,7 @@
  document.getElementById("id_logic").innerHTML = "Logic = 2020.02.02.0";
  
 
-    var busola = document.getElementById("busola");
+    const busola = document.querySelector('.busola');
     navigator.geolocation.watchPosition((data) => {
       console.log(data);
       busola.style.transform = 'rotate(${data.coords.heading}deg)';

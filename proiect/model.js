9212dd219bace17741f1d100e15a9e8fbaff2cc6
gorea98 <56277442+gorea98@users.noreply.github.com>
2020-02-02 15:53:25 +0200

    Add files via upload

--- proiect/model.js
+++ proiect/model.js
@@ -1,1 +1,29 @@
+  function init() {
+        var busola = document.getElementById('busola');
+        if(window.DeviceOrientationEvent) {
+  
+          window.addEventListener('deviceorientation', function(event) {
+                var alpha;
+                //Check for iOS property
+                if(event.webkitCompassHeading) {
+                  alpha = event.webkitCompassHeading;
+                  //Rotation is reversed for iOS
+                  busola.style.WebkitTransform = 'rotate(-' + alpha + 'deg)';
+                }
+                //non iOS
+                else {
+                  alpha = event.alpha;
+                  webkitAlpha = alpha;
+                  if(!window.chrome) {
+                    //Assume Android stock (this is crude, but good enough for our example) and apply offset
+                    webkitAlpha = alpha-270;
+                  }
+                }
  
+                busola.style.Transform = 'rotate(' + alpha + 'deg)';
+                busola.style.WebkitTransform = 'rotate('+ webkitAlpha + 'deg)';
+                //Rotation is reversed for FF
+                busola.style.MozTransform = 'rotate(-' + alpha + 'deg)'; 
+              }, false);
+        }
+      }

4e9769016d58b62d5a6499a1c781d7adeafde198
gorea98 <56277442+gorea98@users.noreply.github.com>
2020-02-02 17:53:57 +0200

    Add files via upload

--- proiect/model.js
+++ proiect/model.js
@@ -4,21 +4,21 @@
 	
 	if(window.DeviceOrientationEvent) {
 		window.addEventListener('deviceorientation', function(event) {
-			var val_alpha;
+			var alpha;
 			
                 //pentru iOS (CompassHeading - diferenta dintre viteza imprejurul axei z si Nord)
                 if(event.webkitCompassHeading) {
 					
-                  val_alpha = event.webkitCompassHeading;
-                  busola.style.WebkitTransform = 'rotate(-' + val_alpha + 'deg)';
+                  alpha = event.webkitCompassHeading;
+                  busola.style.WebkitTransform = 'rotate(-' + alpha + 'deg)';
 				  
                 }
 				
                 // pentru Android
                 else {
 					
-					val_alpha = event.alpha;
-					alphaWebkit = val_alpha;
+					alpha = event.alpha;
+					alphaWebkit = alpha;
 					if(!window.chrome) {
                     
                     webkitAlpha = alpha-270;
@@ -27,8 +27,8 @@
  
 				// Rotirea imaginii
                 busola.style.WebkitTransform = 'rotate('+ alphaWebkit + 'deg)'; // Chrome si Safari
-                busola.style.MozTransform = 'rotate(-' + val_alpha + 'deg)'; // Firefox, rotare inversa
-				busola.style.Transform = 'rotate(' + val_alpha + 'deg)'; // restul browserelor
+                busola.style.MozTransform = 'rotate(-' + alpha + 'deg)'; // Firefox, rotare inversa
+				busola.style.Transform = 'rotate(' + alpha + 'deg)'; // restul browserelor
               }, false);
         }
     

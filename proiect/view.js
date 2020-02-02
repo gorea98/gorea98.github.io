d43bd9c405385a7fa9ae4819bf9439f0cb58e71d
gorea98 <56277442+gorea98@users.noreply.github.com>
2020-02-02 02:41:27 +0200

    Add files via upload

--- proiect/view.js
+++ proiect/view.js
@@ -1,7 +1,7 @@
  document.getElementById("id_logic").innerHTML = "Logic = 2020.02.02.0";
  
 
-    const busola = document.querySelector('.busola');
+    var busola = document.getElementById('busola');
     navigator.geolocation.watchPosition((data) => {
       console.log(data);
       busola.style.transform = 'rotate(${data.coords.heading}deg)';

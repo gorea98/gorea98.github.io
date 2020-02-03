0270a6c690d4932d466ad0eaa793828834516708
gorea98 <56277442+gorea98@users.noreply.github.com>
2020-02-03 02:43:32 +0200

    Add files via upload

--- proiect/model.js
+++ proiect/model.js
@@ -1,3 +1,5 @@
+// SA NU UIT SA FAC READ.ME-UL , Dupa care sa bag js-urile in mape, dupa sa invat codul scris, dupa MVC daca reusesc
+
 document.getElementById("id_logic").innerHTML = "Logic : 2020.02.02.0";
 
 	var busola = document.getElementById('busola');
@@ -33,6 +35,54 @@ document.getElementById("id_logic").innerHTML = "Logic : 2020.02.02.0";
 
 function verifica_versiune()
 {
-	var informatii = document.getElementById('id_UI');
-	alert(informatii.value);
+	var informatii1 = document.getElementById('id_UI');
+	var informatii2 = document.getElementById('id_logic');
+	alert(informatii1.innerHTML + "\n" + informatii2.innerHTML);
 }
+
+//////////////////////////////////////////////////////////////////////
+
+function arata_coordonate()
+{
+	var lat_long = document.getElementById("id_geo");
+	navigator.geolocation.getCurrentPosition(pozitia);
+	
+	function pozitia(x_y) {
+		lat_long.innerHTML = "Latitudine: " + x_y.coords.latitude.toFixed(4) +" "+String.fromCharCode(176)+" N"+"<br>" 
+		+" Longitudine: " + x_y.coords.longitude.toFixed(4) +" "+String.fromCharCode(176)+" E";
+	}
+	
+}
+
+//////////////////////////////////////////////////////////////////////
+
+	var soare = document.getElementById("soare");
+	var ziua = false;
+	
+	soare.addEventListener("click", function()
+	{
+	if(ziua)
+	{
+	document.getElementById("titlu").style.color = "#ffff00";
+	document.getElementById("id_geo").style.color = "#ffffff";
+	document.getElementById("div_busola").style.backgroundColor = "#A9A9A9";
+	document.body.style.backgroundColor = "#A9A9A9";
+	ziua = false;
+	
+	}
+	else
+	{
+	document.getElementById("titlu").style.color = "#006400";
+	document.getElementById("id_geo").style.color = "#800000";
+	document.getElementById("div_busola").style.backgroundColor = "#f5f5dc";
+	document.body.style.backgroundColor = "#f5f5dc";
+	
+	
+	ziua = true;
+	
+	}
+	
+});
+
+
+//////////////////////////////////////////////////////////////////////
